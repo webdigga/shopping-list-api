1a96de66b86a109e0cc7ae3b4988a2ce33ebe419
David White <webdigga42@gmail.com>
2016-12-12 20:55:56 +0000

    Fixing CORS error

--- app.js
+++ app.js
@@ -11,6 +11,12 @@ var recipes = require('./routes/recipes');
 
 var app = express();
 
+app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+});
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -37,13 +43,13 @@ app.use(function(req, res, next) {
 // error handler
 app.use(function(err, req, res, next) {
   
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+	// render the error page
+	res.status(err.status || 500);
+	res.render('error');
 });
 
 // Connect to database
