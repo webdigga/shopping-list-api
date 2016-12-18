84becf4f5f22e5fa94c9e5a87a653c412244de08
David White <webdigga42@gmail.com>
2016-12-18 15:59:37 +0000

    Updating model schema

--- models/recipe.js
+++ models/recipe.js
@@ -2,5 +2,5 @@ var mongoose = require('mongoose');
 var RecipeSchema = new mongoose.Schema({
 	name: String,
 	recipe: String
-});
+}, { collection : 'recipes' });
 module.exports = mongoose.model('Recipe', RecipeSchema);
